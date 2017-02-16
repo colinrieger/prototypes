494ea6d8c6ca4ef9639de9c733f962cc0533eadd
colinrieger <rieger1085@gmail.com>
2017-02-15 21:10:53 -0700

    Setup temporary ground & tank models, tank controls and camera

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject m_Target;
+    Vector3 m_Offset;
+
+    void Start()
+    {
+        m_Offset = m_Target.transform.position - transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = m_Target.transform.eulerAngles.y;
+
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = m_Target.transform.position - (rotation * m_Offset);
+
+        transform.LookAt(m_Target.transform);
+    }
+}
---
+++ Assets/Scripts/TankControls.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControls : MonoBehaviour
+{
+    public float m_Speed = 12f;
+    public float m_TurnSpeed = 180f;
+
+    private Rigidbody m_Rigidbody;
+    private float m_HorizontalInputValue;
+    private float m_VerticalInputValue;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        m_Rigidbody.isKinematic = false;
+        m_HorizontalInputValue = 0f;
+        m_VerticalInputValue = 0f;
+    }
+
+    private void OnDisable()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+
+    void Start ()
+    {
+    }
+
+    void Update ()
+    {
+        m_HorizontalInputValue = Input.GetAxis("Horizontal");
+        m_VerticalInputValue = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        MoveAndTurn();
+    }
+
+    private void MoveAndTurn()
+    {
+        Vector3 movement = transform.forward * m_VerticalInputValue * m_Speed * Time.deltaTime;
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+
+        float turn = m_HorizontalInputValue * m_TurnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+    }
+}
