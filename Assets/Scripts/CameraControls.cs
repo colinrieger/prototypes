706dffae35a6ca66ffae284a25aeca4bc627a90a
colinrieger <rieger1085@gmail.com>
2017-02-23 16:38:01 -0700

    Set the camera offset in code.
    Set the camera offset in code since m_Target may not be set on Start.
    Null check m_Target in FixedUpdate as well.

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -3,15 +3,12 @@
 public class CameraControls : MonoBehaviour
 {
     public GameObject m_Target;
-    Vector3 m_Offset;
-
-    void Start()
-    {
-        m_Offset = m_Target.transform.position - transform.position;
-    }
+    Vector3 m_Offset = new Vector3(0f, -2f, 6f);
 
     private void FixedUpdate()
     {
+        if (!m_Target)
+            return;
         Quaternion rotation = Quaternion.Euler(0, m_Target.transform.eulerAngles.y, 0);
         transform.position = m_Target.transform.position - (rotation * m_Offset);
 
