d48b105f632c384760ca1785f89964ba744e76e1
colinrieger <rieger1085@gmail.com>
2017-10-10 22:05:21 -0600

    Added a look at offset for the camera.
    Added a look at offset for the camera, for a better view angle.

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -3,15 +3,17 @@
 public class CameraControls : MonoBehaviour
 {
     [HideInInspector] public GameObject m_Target;
-    Vector3 m_Offset = new Vector3(0f, -2f, 6f);
+    Vector3 m_PositionOffset = new Vector3(0f, -4f, 8f);
+    Vector3 m_LookAtOffset = new Vector3(0f, 2f, 0f);
 
     private void FixedUpdate()
     {
         if (!m_Target)
             return;
-        Quaternion rotation = Quaternion.Euler(0, m_Target.transform.eulerAngles.y, 0);
-        transform.position = m_Target.transform.position - (rotation * m_Offset);
 
-        transform.LookAt(m_Target.transform);
+        Quaternion rotation = Quaternion.Euler(0, m_Target.transform.eulerAngles.y, 0);
+        transform.position = m_Target.transform.position - (rotation * m_PositionOffset);
+
+        transform.LookAt(m_Target.transform.position + m_LookAtOffset);
     }
 }
