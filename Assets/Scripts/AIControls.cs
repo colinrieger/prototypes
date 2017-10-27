ae74472ff0f5b7dedfa00667680a01c61b6f79b2
colinrieger <rieger1085@gmail.com>
2017-10-27 14:23:42 -0600

    SphereCast at target tank's turret.
    -Use a SphereCast pointing at the target tank's turret, instead of a RayCast pointing at the main tank transform. RayCast can hit the target tank while the shell may not be able to.

--- Assets/Scripts/AIControls.cs
+++ Assets/Scripts/AIControls.cs
@@ -8,16 +8,19 @@ public class AIControls : MonoBehaviour
     private Rigidbody m_TankRigidbody;
     private TankControls m_TankControls;
     private GameObject m_TankTurret;
+    private GameObject m_TargetTankTurret;
     private NavMeshAgent m_NavMeshAgent;
 
     private const float c_MaxAngleToTargetToMove = 10f;
     private const float c_DistanceToTargetOffset = 20f;
+    private const float c_ShellRadius = 0.2f;
 
     private void Start()
     {
         m_TankRigidbody = GetComponent<Rigidbody>();
         m_TankControls = GetComponent<TankControls>();
         m_TankTurret = transform.Find("Renderers/Turret").gameObject;
+        m_TargetTankTurret = m_TargetTank.transform.Find("Renderers/Turret").gameObject;
 
         m_NavMeshAgent = GetComponent<NavMeshAgent>();
         m_NavMeshAgent.speed = m_TankControls.m_Speed;
@@ -29,8 +32,11 @@ public class AIControls : MonoBehaviour
 
     private void FixedUpdate()
     {
-        UpdateTank();
-        UpdateTurret();
+        if (m_TargetTank.activeSelf)
+        {
+            UpdateTank();
+            UpdateTurret();
+        }
     }
 
     private void UpdateTank()
@@ -56,19 +62,16 @@ public class AIControls : MonoBehaviour
 
     private void UpdateTurret()
     {
-        Vector3 targetDirection = (m_TargetTank.transform.position - m_TankTurret.transform.position);
+        Vector3 targetDirection = (m_TargetTankTurret.transform.position - m_TankTurret.transform.position);
         Quaternion rotationToTarget = Quaternion.LookRotation(targetDirection);
         rotationToTarget.x = 0;
         rotationToTarget.z = 0;
 
         Quaternion turretRotation = Quaternion.RotateTowards(m_TankTurret.transform.rotation, rotationToTarget, Time.deltaTime * m_TankControls.m_TurretRotationSpeed);
         m_TankTurret.transform.rotation = turretRotation;
-        
-        if (m_TargetTank.activeSelf)
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(m_TankTurret.transform.position, targetDirection, out hit) && hit.transform == m_TargetTank.transform)
-                m_TankControls.Fire();
-        }
+
+        RaycastHit hit;
+        if (Physics.SphereCast(m_TankTurret.transform.position, c_ShellRadius, targetDirection, out hit) && hit.transform == m_TargetTank.transform)
+            m_TankControls.Fire();
     }
 }
