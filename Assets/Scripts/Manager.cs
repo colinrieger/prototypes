59f83cef4c1c06487768b5245599203a55c6d80f
colinrieger <rieger1085@gmail.com>
2017-11-30 23:08:53 -0700

    Added a pause menu.

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -6,9 +6,12 @@ public class Manager : MonoBehaviour
     public GameObject m_TankPrefab;
     public Transform m_PlayerSpawn;
     public Transform m_AISpawn;
+    public GameObject m_PauseMenu;
 
     private GameObject m_PlayerTank;
     private GameObject m_AITank;
+
+    private bool m_IsPaused = false;
 
     private void Start()
     {
@@ -17,6 +20,18 @@ public class Manager : MonoBehaviour
 
         SpawnPlayerTank();
         SpawnAITank();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            m_IsPaused = !m_IsPaused;
+            Time.timeScale = m_IsPaused ? 0 : 1;
+            m_PauseMenu.SetActive(m_IsPaused);
+            Cursor.visible = m_IsPaused;
+            Cursor.lockState = m_IsPaused ? CursorLockMode.None : CursorLockMode.Locked;
+        }
     }
 
     private void SpawnPlayerTank()
